2b6d155d3f727461960181d9b053c3708a08027e
Chladni <bucketofjava@gmail.com>
2025-11-04 13:27:44 -0600

    cleaned up code + annotated

--- experiments/glucose_tests/scaletst_autoregress.py
+++ experiments/glucose_tests/scaletst_autoregress.py
@@ -1,7 +1,6 @@
 from typing import List, Tuple, Optional, Union, Dict
 from neuralforecast.losses.pytorch import BasePointLoss, DistributionLoss
 import math
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -15,16 +14,22 @@ from neuralforecast.losses.pytorch import MAE, BasePointLoss
 from neuralforecast.common._base_model import BaseModel
 from neuralforecast.common._modules import RevIN
 from neuralforecast.losses.pytorch import MAE
+#VQVAE implemented in vqvae.py
 from vqvae import train_and_get_VQVAE, train_and_get_VQVAEConv
 from neuralforecast.models.patchtst import positional_encoding, TSTEncoder
 import torch
+#Transformer implemented through xtransformers
 from x_transformers import XTransformer, TransformerWrapper, Decoder, AutoregressiveWrapper
+
+
 from utilsforecast.data import generate_series
 from utilsforecast.evaluation import evaluate
 from utilsforecast.losses import mae
 from utilsforecast.plotting import plot_series
 #Values: N - batch size. L - Sequence Length. F - Future Horizon size. P - Patch Size. H - Hidden Size.
-#So we can just have model output its own loss and be fine
+
+#Because of how neuralforecast does loss I added this, it allows for both doing crossentropy loss and allows for custom loss computation at
+#multiple scales without having to mess with how the data is formatted
 class IdentityLoss(BasePointLoss):
     def __init__(self, horizon_weight=None):
         super(IdentityLoss, self).__init__(
@@ -38,18 +43,20 @@ class IdentityLoss(BasePointLoss):
         y_insample: Union[torch.Tensor, None] = None,
     ) -> torch.Tensor:
         return y_hat
+
 #Tokenization:
-#Define tokenizer abstract class
+#Tokenizer abstract class
+#Converts patches to indices and back
 class IndexTokenizer():
     def __init__(self, patch_size, **kwargs):
-        #super(IndexTokenizer, self).__init__()
         self.patch_size=patch_size
         self.codebook_size=0
     def get_indices(self, patched_data):
         pass
     def indices_to_values(self, indices):
         pass
-#Define VQVAE
+
+#VQVAETokenizer
 class VQVAETokenizer(IndexTokenizer):
     def __init__(self, patch_size, **kwargs):
         super().__init__(patch_size, **kwargs)
@@ -60,13 +67,15 @@ class VQVAETokenizer(IndexTokenizer):
     def get_indices(self, patched_data, index=0):
         # Assumed shape of patches: (N, L//P, P)
         tokenized_indices=self.model.get_quantized(patched_data)[1]
+        #We dont want tokens to overlap, since these are diff VQVAEs they will have colliding indices
+        #so we add the index times the codebook size to make the indices distinct
         tokenized_indices+=index*self.codebook_size
         return tokenized_indices
     def indices_to_values(self, indices, index):
         real_vals=self.model.get_actual_from_indices(indices%150)
-      #  print(f"Real vals size: {real_vals.size()}")
         return real_vals
-#Define a simple tokenizer that just takes the mean of the values in a patch and tokenizes as 1 0 -1s
+
+#A simple tokenizer that just takes the mean of the values in a patch and tokenizes as 1 0 -1s, meant for smoke testing and not real imp#
 class MeanSignTokenizer(IndexTokenizer):
     def __init__(self, patch_size, **kwargs):
         super().__init__(patch_size, **kwargs)
@@ -92,7 +101,7 @@ class AutoregressiveScaleModel(BaseModel):
     RECURRENT = (
         False  # If the model produces forecasts recursively (True) or direct (False)
     )
-#Init method: call super init, store needed variables, initialize tokenizers and transformer
+    #Init method: call super init, store needed variables, initialize tokenizers and transformer
     def __init__(self,
                 h,
                 input_size,
@@ -165,18 +174,23 @@ class AutoregressiveScaleModel(BaseModel):
             dataloader_kwargs=dataloader_kwargs,
             **trainer_kwargs
         )
+        #Reversible Ins. Normalization
         self.revin_module=RevIN(1, affine=False)
+        #Forecast Horizon
         self.h=h
+        #Input Size
         self.input_size=input_size
+        #Class of the tokenizer (Mean, VQVAE, etc.)
         self.tokenizer_class=tokenizer_class
+        #List of patch sizes
         self.patch_sizes=patch_sizes
+        #Smallest patch size, which is what finally gets converted from index -> data
         self.patch_small=patch_sizes[-1]
+        #Tokenizers
         self.tokenizers=[(tokenizer_class(ps, **tokenizer_kwargs) if tokenizer_kwargs!=None else tokenizer_class(ps)) for ps in self.patch_sizes]
-       # self.tokenizers=nn.ModuleList(self.tokenizer_list)
-        # for tokenizer in self.tokenizers:
-        #     for p in tokenizer.parameters():
-        #         p.requires_grad_=False
+        #Codebook size of each tokenizer
         self.codebook_size=self.tokenizers[0].codebook_size
+        #Number of tokens; sum of the codebook sizes
         num_tokens=int(sum([t.codebook_size for t in self.tokenizers])*1.2)
         self.transformer_model=TransformerWrapper(
             num_tokens=num_tokens,
@@ -188,25 +202,27 @@ class AutoregressiveScaleModel(BaseModel):
             attn_flash=True
             )
         ).cuda()
-#Wrap transformer in autoregressive wrapper
+        #Wrap transformer in autoregressive wrapper
         self.transformer_model=AutoregressiveWrapper(self.transformer_model)
-    #TODO? Doesn't seem necessary ATM
-#Training tokenizer method. For now can just be VQVAE and a pass on the mean one. Important: take in patch index and add codebook_size*patch_index to indices to ensure unique indices!
+    #Training tokenizer method. For now just VQVAE and a pass on the mean one. 
     def train_tokenizer(self, training_data):
         if(self.tokenizer_class!=VQVAETokenizer):
             return
         for i in range(len(self.tokenizers)):
+            #Make sure the input data is divisible by the patch size
             training_data_base=training_data[(training_data.shape[0]%self.patch_sizes[i]):].unsqueeze(-1)
             training_data_=training_data_base.reshape(-1, self.patch_sizes[i])
             for j in range(1, self.patch_sizes[i]):
+                #Rotate the sequence to allow for more robust patching (ie get patches starting at every value mod P)
                 training_data_=torch.cat((training_data_, torch.cat((training_data_base[j:], training_data_base[:j]), dim=0).reshape(-1, self.patch_sizes[i])), dim=0)
-        #    print("Passing in size: {}".format(training_data_.size()))
+            #Train each individual vqvae on the patched training data
             self.tokenizers[i].train_vqvae(training_data_)
 
-#Forecasting step: Take in an index or patch size, as well as the current token sequence. Output the log probs for the next token in the sequence
-#Repeat until stretched out over forecasting horizon with the patches
+    #Forecasting step: Take in an index or patch size, as well as the current token sequence. Output the log probs for the next token in the sequence
+    #Repeat until stretched out over forecasting horizon with the patches
     def forecast_step(self, indices, patch_size, mask_index=1, return_loss=False):
         logits=self.transformer_model.net(indices[:, :-1])
+        #If returning loss (training) then mask and compute the loss
         if(return_loss):
             target=indices[:, 1:].clone().long()
             target[:, :mask_index]=-100
@@ -216,60 +232,43 @@ class AutoregressiveScaleModel(BaseModel):
                 ignore_index=-100
             )
             return loss, logits[mask_index:]
+        #Otherwise just return the new logits
         return logits[mask_index:]
 
-#Core model loop: TRAINING
     def forward(self, windows_batch):
-      #  print(f"Size of insample: {windows_batch['insample_y'].size()}")
         real_data=windows_batch['insample_y']
-     #   real_data.requires_grad_=False
         cur_seq=None
         tot_loss=0
         for i in range(len(self.patch_sizes)):
-       #     print(f"RD size: {real_data.size()}")
+            #Patch data
             real_data_=real_data.unsqueeze(-1).reshape((real_data.size()[0], -1,patch_sizes[i]))
-          #  print(f"RD_ size: {real_data_.size()}")
-            #Should return a sequence of longs
+            #Tokenize data
             tokenized=(self.tokenizers[i].get_indices(real_data_, i))
-          #  print(f"T size: {tokenized.size()}")
+            #Add it to the current sequence of tokens
             if(cur_seq==None):
                 indices=tokenized
                 cur_seq=indices
             else:
                 indices=torch.cat((cur_seq, tokenized), dim=1)
-           # indices=indices.detach()
+            # If training, run forecast, get loss
             if(self.training):
                 loss=self.forecast_step(indices, self.patch_sizes[i], mask_index=max(indices.shape[0]-tokenized.shape[0], 1), return_loss=True)[0]
                 tot_loss=tot_loss+loss
             else:
+                #Otherwise get new indices
                 cur_seq=self.transformer_model.generate(indices, seq_len=self.h // self.patch_small)
-            # cur_seq=cur_seq.detach()
+        #If training we just care about the loss
         if(self.training):
             return tot_loss
-     #   print(f"Sequence length: {cur_seq.size()[0]}")
-        print(f"Indices: {cur_seq[:, -(self.h // self.patch_small):]}")
+        #Get the indices of the final values on the smallest patch size for conversion to real data
         Y=self.tokenizers[-1].indices_to_values(cur_seq[:, -(self.h // self.patch_small):], len(self.tokenizers)-1).flatten(-2, -1)
+        #RevIN is disabled for now so I can test more easily - it messes with tokenization
         #return self.revin_module(Y.unsqueeze(-1), "denorm").reshape(Y.size())
         return Y
 
                 
             
-#Revin everything
-#Take in train, test data
-#Loop through patch sizes
-#Compute token indices for the i-th patch, and append to the current sequence
-#Pass into the model 
-#Get the output logits, run crossentropyloss with the 'true' tokens as determined by the real test data
-#Append to the token sequence, detach everything from grad, and pass back in for the next batch
-#Un-revin the final result for inference
-
-#Core model loop: EVAL
-#Just do the above but don't pass in shit
-#Maybe add a training parameter to the above that runs it like normal and then otherwise doesn't wow
-
-#Actual training step
-#Overload pytorch lightning to run the model and pass in the optimizer and loss and do it w/i model!
-
+#Minor testing
 if __name__ == "__main__":
     print("Testing the model")
     HORIZON=18
@@ -279,7 +278,6 @@ if __name__ == "__main__":
     ar_model.train()
     Y_df = generate_series(n_series=100, min_length=100, max_length=200)
     torch.set_float32_matmul_precision('medium')
-    # Split into train and test
     Y_train_df = Y_df.groupby('unique_id').head(-HORIZON)
     Y_test_df = Y_df.groupby('unique_id').tail(HORIZON)
     print(f"Train length: {len(Y_train_df)}")
@@ -302,9 +300,6 @@ if __name__ == "__main__":
         on=['ds'], 
         how='left'
     )
-    
-    # Calculate MAE for each model
-    # This dataframe has the MAE per unique_id
     evaluation_df = evaluate(
         Y_test_with_preds_df, 
         metrics=[mae],
